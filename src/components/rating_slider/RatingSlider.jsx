627fda2260af4d9667655a8eec4ab8aec6486087
nazarelis <anabelojeda2001@gmail.com>
2024-05-27 23:10:22 -0300

    cambios en css slider rating

--- src/components/rating_slider/RatingSlider.jsx
+++ src/components/rating_slider/RatingSlider.jsx
@@ -3,9 +3,6 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import UserCommentComponent from '../user_comment/UserComment';
 import './RatingSlider.css';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
 
 const testimonialData = [
   {
@@ -22,8 +19,6 @@ const testimonialData = [
   }
 ];
 
-
-
 const settings = {
   dots: true,
   infinite: true,
@@ -36,22 +31,35 @@ const settings = {
   centerPadding: '0px',
   className: 'slider_container',
 };
- 
+
 const RatingSlider = () => {
   return (
     <div className='rating_slider_container'>
-            <div style={{ width: '80%', margin: '0 auto' }}>
-      <Slider {...settings}>
-        {testimonialData.map(item => (
-          <div key={item.id}>
-            {item.component}
-          </div>
-        ))}
-      </Slider>
-    </div>
+      <div className='background-container'>
+        <div className='background-image-left-rating-slider'>
+          <img src="src/assets/images/Group left 399.png" alt="Left Background" />
+        </div>
+        <div className='background-image-right-rating-slider'>
+          <img src="src/assets/images/Group right 398.png" alt="Right Background" />
+        </div>
+        <div className='background-image-top-rating-slider'>
+          <img src="src/assets/images/Group 399.png" alt="Top Background" />
+        </div>
+        <div className='background-image-bottom-rating-slider'>
+          <img src="src/assets/images/Group 398.png" alt="Bottom Background" />
+        </div>
+      </div>
 
+      <div style={{ width: '80%', margin: '0 auto' }}>
+        <Slider {...settings}>
+          {testimonialData.map(item => (
+            <div key={item.id}>
+              {item.component}
+            </div>
+          ))}
+        </Slider>
+      </div>
     </div>
-
   );
 }
 

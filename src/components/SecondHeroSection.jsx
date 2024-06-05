011f0c42d15fc80840a603af464c2425d46086a8
gpedrosad <gpedrosa@uade.edu.ar>
2024-06-05 13:18:52 -0300

    cambios en el about y gradiente del hero section

--- src/components/SecondHeroSection.jsx
+++ src/components/SecondHeroSection.jsx
@@ -6,6 +6,7 @@ const HeroSection = ({ imageSrc, title }) => {
         <div className="relative w-full h-95 md:h-128 lg:h-160 overflow-hidden">
             <Navbar />
             <img src={imageSrc} alt="Hero Background" className="w-full h-full object-cover" />
+            <div className="absolute inset-0 bg-gradient-to-br from-[#00000000] via-[#00000099] to-[#00000000]"></div>
             <div className="absolute inset-0 flex flex-col items-center justify-center text-center p-8 md:p-12">
                 <h1 className="font-rufina text-new-green text-4xl md:text-6xl lg:text-8xl font-bold">{title}</h1>
             </div>

6c70bd7e41f0c204ce53a1383d3fe7521a50eda1
gpedrosad <gpedrosa@uade.edu.ar>
2024-06-29 23:45:35 -0300

    footer y avance

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,16 +7,16 @@ const Footer = () => {
     <>
       <div className="bg-custom-green grid grid-cols-1 md:grid-cols-3 gap-4">
     <div className="flex flex-col items-center justify-center">
-        <img src={logo} className="w-3/6"/>
+        <img src={logo} className="md:w-3/6 mt-10"/>
     </div>
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col md:ml-36 xs:items-center md:items-start justify-center">
         <h1 className="font-rufina font-bold text-white text-3xl sm:text-3xl md:text-3xl xl:text-4xl pb-5">Contact</h1>
         <p className="text-white text-xl md:text-md xl:text-xl">+1 86 852 346 000</p>
         <p className="text-white mb-10 text-xl md:text-md xl:text-xl">info@lakebarrine.com</p>
         <span className="text-white text-xl md:text-md xl:text-xl">Lake Barrine.</span>
         <span className="text-white text-xl md:text-md xl:text-xl">Australia, 90230</span>
     </div>
-    <div className="flex flex-col items-start justify-center p-16 sm:pt-3 md:pt-24">
+    <div className="flex flex-col xs:items-center md:items-start justify-center p-16 sm:pt-3 md:pt-24">
         <h1 className="whitespace-nowrap font-rufina font-bold text-white text-3xl md:text-3xl xl:text-3xl pb-5">Opening hours</h1>
         <p className="sm:mr-0 text-white sm:text-xl md:text-md xl:text-xl">Monday to Sunday</p>
         <p className="sm:mr-0 text-white sm:text-xl md:text-md xl:text-xl">9:00 am - 10:00 pm</p>
--- src/components/ImageText.jsx
+++ src/components/ImageText.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 
 const ImageText = () => {
   return (
-    <div className="">
-      <h1 className="text-custom-green font-rufina font-bold py-6 xl:text-5xl 2xl:text-6xl md:text-2xl lg:text-3xl xs:text-3xl ">Celebrate your special event <span class="text-yellow-500">under the stars</span>, surrounded by lush rainforest</h1>
-      <p className="py-4 text-xl md:text-xl lg:text-2xl xl:text-3xl text-custom-green 2xl:text-4xl">Immerse yourself in the warm embrace of our indoor oasis, savor al-fresco moments on our charming outdoor terrace, or unwind at the stylish bar where crafted libations await. Discover a trifecta of atmospheres that elevate your dining journey.</p>
+    <div className="sm:mb-36 md:mb-0">
+      <h1 className="text-custom-green font-rufina font-bold py-6 xl:text-5xl 2xl:text-6xl sm:text-lg md:text-3xl lg:text-3xl xs:text-3xl ">Celebrate your special event <span class="text-yellow-500">under the stars</span>, surrounded by lush rainforest</h1>
+      <p className="py-4 text-xl sm:text-lg md:text-xl lg:text-2xl xl:text-3xl text-custom-green 2xl:text-4xl">Immerse yourself in the warm embrace of our indoor oasis, savor al-fresco moments on our charming outdoor terrace, or unwind at the stylish bar where crafted libations await. Discover a trifecta of atmospheres that elevate your dining journey.</p>
       <div className="py-4">
 
     <Link to='/functions'>

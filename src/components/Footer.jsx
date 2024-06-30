9057c0d2652435a9dd1a911c44c12a93c397ebb5
gpedrosad <gpedrosa@uade.edu.ar>
2024-06-29 21:03:46 -0300

    cambios en footer y contenido contact

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,17 +10,17 @@ const Footer = () => {
         <img src={logo} className="w-3/6"/>
     </div>
     <div className="flex flex-col items-center justify-center">
-        <h1 className="font-rufina font-bold text-white text-3xl sm:text-3xl md:text-2xl pb-5">Contact</h1>
-        <p className="text-white text-xl md:text-sm">+1 86 852 346 000</p>
-        <p className="text-white mb-10 text-xl md:text-sm">info@lakebarrine.com</p>
-        <span className="text-white text-xl md:text-sm">Lake Barrine.</span>
-        <span className="text-white text-xl md:text-sm">Australia, 90230</span>
+        <h1 className="font-rufina font-bold text-white text-3xl sm:text-3xl md:text-3xl pb-5">Contact</h1>
+        <p className="text-white text-xl md:text-md">+1 86 852 346 000</p>
+        <p className="text-white mb-10 text-xl md:text-md">info@lakebarrine.com</p>
+        <span className="text-white text-xl md:text-md">Lake Barrine.</span>
+        <span className="text-white text-xl md:text-md">Australia, 90230</span>
     </div>
     <div className="flex flex-col items-start justify-center p-16 sm:pt-3 md:pt-24">
-        <h1 className="whitespace-nowrap font-rufina font-bold text-white text-3xl md:text-xl pb-5">Opening hours</h1>
-        <p className="sm:mr-0 text-white sm:text-xl md:text-sm">Monday to Sunday</p>
-        <p className="sm:mr-0 text-white sm:text-xl md:text-sm">9:00 am - 10:00 pm</p>
-        <h1 className="whitespace-nowrap font-rufina font-bold text-white text-3xl sm:text-3xl md:text-3xl md:pt-10 sm:pb-3 sm:pt-12 md:text-xl mt-4 xs:pt-6">Stay connected</h1>
+        <h1 className="whitespace-nowrap font-rufina font-bold text-white text-3xl md:text-2xl pb-5">Opening hours</h1>
+        <p className="sm:mr-0 text-white sm:text-xl md:text-md">Monday to Sunday</p>
+        <p className="sm:mr-0 text-white sm:text-xl md:text-md">9:00 am - 10:00 pm</p>
+        <h1 className="whitespace-nowrap font-rufina font-bold text-white text-3xl sm:text-3xl md:text-2xl md:pt-10 sm:pb-3 sm:pt-12 mt-4 xs:pt-6">Stay connected</h1>
           <SocialMediaIcons/>
         </div>
       </div>

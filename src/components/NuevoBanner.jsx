fb732e6878e09e8dface679b5e22989cf61c1a24
gpedrosad <gpedrosa@uade.edu.ar>
2024-07-01 12:36:05 -0300

    cambios nalita

--- src/components/NuevoBanner.jsx
+++ src/components/NuevoBanner.jsx
@@ -15,23 +15,24 @@ import ramader from '/public/images/ramader.svg';
 const NuevoBanner = () => {
   return (
     <div className="relative w-full bg-[#B8CAA8] grid grid-cols-1 lg:grid-cols-10 lg:grid-rows-2">
-  <img src={hojaIzq} alt="Descripción de la imagen" className="absolute top-0 left-0 w-20 h-20 lg:w-30 lg:h-30" />
+  <img src={hojaIzq} alt="Descripción de la imagen" className="absolute top-0 left-0 w-20 h-20 lg:w-40 lg:h-40" />
   
-  <div className="relative col-start-1 lg:col-start-1 lg:col-span-4 lg:row-start-1 lg:row-span-2 flex justify-center lg:flex-none lg:justify-start xl:mb-20">
-  <img src={nalita2} className="shadow-none md:m-20 object-contain" />
-</div>
-
-  <div className="lg:col-start-6 lg:col-span-4 lg:row-start-1 lg:row-span-1 lg:mt-40 relative">
-    <div className="grid grid-cols-3">
-      <img src={ramaizq} alt="Descripción de la imagen" className="w-20 h-20 lg:ml-24 ml-48 mb-10" />
-      <h1 className="lg:col-start-2 whitespace-nowrap font-rufina font-bold text-2xl text-third-green md:text-3xl lg:text-5xl text-center">Our Story</h1>
-      <img src={ramader} alt="Descripción de la imagen" className="w-20 h-20 lg:col-start-3 lg:ml-10" />
-    </div>
-    <span className="font-rufina font-bold text-lg text-custom-green md:text-xl lg:text-2xl"></span>
-    <p className="font-inika font-normal text-xl lg:text-2xl mt-5 p-20 lg:p-0">
-      As you walk around our beautiful gardens, stroll through our historic Teahouse and dip your toes in the natural waters of Lake Barrine, you might find, just like I did, that falling in love with this place comes easy.
-    </p>
+  <div className="flex justify-center items-center lg:col-start-1 lg:col-span-6 lg:row-start-1 lg:row-span-2 lg:mb-40 mt-10 lg:mt-0">
+    <img src={nalita2} className="shadow-none" />
   </div>
+
+<div className="lg:col-start-6 lg:col-span-4 lg:row-start-1 lg:row-span-1 lg:mt-40 relative">
+  <div className="flex items-center justify-center mb-10 space-x-1">
+    <img src={ramaizq} alt="Descripción de la imagen" className="w-15 h-15" />
+    <h1 className="p-10 font-rufina font-bold text-2xl text-third-green md:text-4xl lg:text-5xl text-center">Where the Rainforest meets the Lake</h1>
+    <img src={ramader} alt="Descripción de la imagen" className="w-15 h-15" />
+  </div>
+
+  <span className="font-rufina font-bold text-lg text-custom-green md:text-xl lg:text-2xl"></span>
+  <p className="font-inika font-normal text-xl md:text-2xl lg:text-3xl mt-5 p-20 lg:p-0">
+    As you walk around our beautiful gardens, stroll through our historic Teahouse and dip your toes in the natural waters of Lake Barrine, you might find, just like I did, that falling in love with this place comes easy.
+  </p>
+</div>
 
   <div className="mt-10 lg:mt-72 lg:ml-20 col-start-1 lg:col-start-2 lg:col-span-5 lg:row-start-2 lg:row-span-4 w-full border-t-4 border-new-green"></div>
 

1d5c115722695902b544b23e488e0b459fc49820
gpedrosad <gpedrosa@uade.edu.ar>
2024-07-02 13:51:02 -0300

    nalita listo casi

--- src/components/NuevoBanner.jsx
+++ src/components/NuevoBanner.jsx
@@ -14,33 +14,33 @@ import ramader from '/public/images/ramader.svg';
 
 const NuevoBanner = () => {
   return (
-  <div className="relative bg-[#B8CAA8] grid grid-cols-10 grid-rows-8 gap-5">
+  <div className="relative bg-[#B8CAA8] grid grid-cols-1 lg:grid-cols-10 g:grid-rows-8 gap-5">
     <img src={hojaIzq} alt="Descripción de la imagen" className="absolute top-0 left-0 w-20 h-20 lg:w-30 lg:h-30" />
-    <div className="col-span-4 row-span-5 col-start-2 row-start-2">
-      <img src={nalita2} className="shadow-none p-10 lg:p-0" />
+    <div className="col-start-1 lg:col-span-4 lg:row-span-5 lg:col-start-2 lg:row-start-2 flex md:flex-none justify-center">
+      <img src={nalita2} className="shadow-none p-10 lg:p-0 mt-10" />
     </div>
-  <div className="col-span-4 row-span-4 col-start-6 row-start-2 xl:row-start-2">
-  <div className="flex flex-col items-center justify-center lg:mb-10 lg:mt-16">
+  <div className="lg:col-span-4 lg:row-span-4 lg:col-start-6 lg:row-start-2 lg:xl:row-start-2">
+  <div className="flex flex-col items-center justify-center lg:mb-10 lg:mt-24">
   <div className="flex items-center space-x-1">
     <img src={ramaizq} alt="Descripción de la imagen" className="w-15 h-15" />
-    <h1 className="p-2 font-rufina font-bold text-2xl text-third-green md:text-3xl lg:text-4xl text-center">
+    <h1 className="p-2 font-rufina font-bold text-3xl text-third-green md:text-4xl lg:text-5xl text-center">
       Where the Rainforest
     </h1>
     <img src={ramader} alt="Descripción de la imagen" className="w-15 h-15" />
     </div>
-    <h1 className="p-2 font-rufina font-bold text-2xl text-third-green md:text-3xl lg:text-4xl text-center">
+    <h1 className="p-2 font-rufina font-bold text-3xl text-third-green md:text-4xl lg:text-5xl text-center">
       meets the Lake
     </h1>
   </div>
-      <p className="font-inika font-normal text-xl md:text-2xl lg:text-3xl">
+      <p className="font-inika font-normal text-lg md:text-xl lg:text-2xl p-20 lg:p-0">
         As you walk around our beautiful gardens, stroll through our historic Teahouse and dip your toes in the natural waters of Lake Barrine, you might find, just like I did, that falling in love with this place comes easy.
       </p>
     </div>
-    <div className="col-span-6 col-start-2 row-start-7">
+    <div className="lg:col-span-6 lg:col-start-2 lg:row-start-7">
       <div className="w-full border-t-4 border-new-green mt-8"></div>
     </div>
 
-  <div className="col-span-2 col-start-9 row-start-7">
+  <div className="col-start-1 lg:col-span-2 lg:col-start-8 lg:row-start-7 flex justify-center mb-10 lg:flex-none lg:justify-start lg:ml-20">
     <Link to='/about'>
       <TransparentButton className="" text="About us" />
     </Link>

26f11851554a3740136fae049363b9fa8e9d8736
gpedrosad <gpedrosa@uade.edu.ar>
2024-07-03 00:51:12 -0300

    cambio quote

--- src/components/QuoteSection.jsx
+++ src/components/QuoteSection.jsx
@@ -5,7 +5,7 @@ const QuoteSection = ({ quote, author }) => {
   return (
     <div className="relative flex items-center justify-center h-1/3 min-h-1/3 bg-[#F7F7F7] p-4 mx-auto" style={{ minHeight: '33vh' }}>
       <div className="text-center mx-auto mt-16">
-        <p className="text-lg md:text-2xl xl:text-3xl text-custom-green font-semibold px-80">
+        <p className="px-0 md:text-2xl xl:text-3xl text-custom-green font-semibold md:px-80">
           {quote}
         </p>
         <p className="text-md md:text-lg xl:text-xl text-custom-green font-medium mt-4">

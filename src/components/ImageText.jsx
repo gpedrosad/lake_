e66db44503e1f01e82a90891c69f60bdf59dfc0b
gpedrosad <gpedrosa@uade.edu.ar>
2024-07-02 23:28:54 -0300

    cambios en textos

--- src/components/ImageText.jsx
+++ src/components/ImageText.jsx
@@ -5,16 +5,16 @@ import { Link } from 'react-router-dom';
 const ImageText = () => {
   return (
     <div className="sm:mb-36 md:mb-0">
-      <h1 className="text-custom-green font-rufina font-bold py-6 xl:text-5xl 2xl:text-6xl sm:text-lg md:text-3xl lg:text-3xl xs:text-3xl ">Celebrate your special event <span class="text-yellow-500">under the stars</span>, Where moments become unforgettable memories</h1>
-      <p className="py-4 text-xl sm:text-lg md:text-xl lg:text-2xl xl:text-3xl text-custom-green 2xl:text-4xl">From cosy veranda chats over a hot cup of tea, to gathering with your favourite people and watching the sunset from the deck of our boat, Lake Barrine has something for everyone.</p>
-      <p className="py-4 text-xl sm:text-lg md:text-xl lg:text-2xl xl:text-3xl text-custom-green 2xl:text-4xl">Then underneath that please change the 5km wording to:</p>
-      <p className="py-4 text-xl sm:text-lg md:text-xl lg:text-2xl xl:text-3xl text-custom-green 2xl:text-4xl">Explore the Rainforest with our 5km walking track through beautiful century old National
+      <h1 className="text-custom-green font-rufina font-bold py-6 xl:text-3xl 2xl:text-4xl sm:text-lg md:text-3xl lg:text-3xl xs:text-3xl ">Celebrate your special event <span class="text-yellow-500">under the stars</span>, Where moments become unforgettable memories</h1>
+      <p className="py-4 text-xl sm:text-lg md:text-lg lg:text-xl xl:text-xl text-custom-green 2xl:text-2xl">From cosy veranda chats over a hot cup of tea, to gathering with your favourite people and watching the sunset from the deck of our boat, Lake Barrine has something for everyone.</p>
+      <p className="text-xl sm:text-lg md:text-lg lg:text-xl xl:text-xl text-custom-green 2xl:text-2xl">Then underneath that please change the 5km wording to:</p>
+      <p className="text-xl sm:text-lg md:text-lg lg:text-xl xl:text-xl text-custom-green 2xl:text-2xl">Explore the Rainforest with our 5km walking track through beautiful century old National
 Park.</p>
 
       <div className="py-4">
 
     <Link to='/functions'>
-    <button className="w-48 h-20 bg-[#3C5220] text-white rounded-lg text-lg cursor-pointer" onclick="window.location.href='URL_AQUI'">
+    <button className="w-48 h-16 bg-[#3C5220] text-white rounded-lg text-lg cursor-pointer" onclick="window.location.href='URL_AQUI'">
       Explore</button>
       </Link>
     </div>

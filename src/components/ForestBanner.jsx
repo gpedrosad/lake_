6b681afc727358f8393feeddd67f9bcc2c70a83e
gpedrosad <gpedrosa@uade.edu.ar>
2024-07-05 20:46:02 -0300

    cambios felipe

--- src/components/ForestBanner.jsx
+++ src/components/ForestBanner.jsx
@@ -10,16 +10,11 @@ const ForestBanner = () => {
       <img className="absolute inset-0 w-full h-full object-cover object-right-bottom" src={arboles} alt="Árboles" />
       <div className="absolute inset-0 bg-custom-green opacity-50"></div>
       <div className="absolute inset-0 z-10 p-6 flex items-center justify-center">
-        <img src={hojablanca1} alt="Descripción de la imagen" style={{ width: '60px', height: '60px' }} className="m-5 mb-40 md:mb-40 xl:mb-48 2xl:mb-40"/>
+        <img src={hojablanca1} alt="Descripción de la imagen" style={{ width: '60px', height: '60px' }} className="m-5 mb-40 md:mb-40 xl:mb-10 2xl:mb-40"/>
         <h1 className="text-white md:text-5xl sm:text-4xl font-rufina font-bold text-center mt-14 xl:mt-4 xs:text-4xl">
-          Explore the rainforest in a walk of 5 km around the lake
-        <Link to='/accomodation'>
-        <div className="flex justify-center pt-20">
-          <YellowButton text="Book"/>
-        </div>
-        </Link>
-        </h1>
-        <img src={hojablanca2} alt="Descripción de la imagen" style={{ width: '50px', height: '50px' }} className="m-5 mb-40 xl:mb-48 2xl:mb-36"/>
+        Explore the Rainforest with our 5km walking track through beautiful century old National
+Park.</h1>
+        <img src={hojablanca2} alt="Descripción de la imagen" style={{ width: '50px', height: '50px' }} className="m-5 mb-40 xl:mb-12 2xl:mb-36"/>
       </div>
       <div>
       </div>
--- src/components/ImageText.jsx
+++ src/components/ImageText.jsx
@@ -5,13 +5,11 @@ import { Link } from 'react-router-dom';
 const ImageText = () => {
   return (
     <div className="sm:mb-36 md:mb-0">
-      <h1 className="text-custom-green font-rufina font-bold py-6 xl:text-3xl 2xl:text-5xl sm:text-xl md:text-3xl lg:text-3xl xs:text-3xl ">Celebrate your special event <span class="text-yellow-500">under the stars</span>, Where moments become unforgettable memories</h1>
-      <p className="py-4 text-sm sm:text-md md:text-md lg:text-lg xl:text-xl 2xl:text-2xl text-custom-green 2xl:text-3xl">From cosy veranda chats over a hot cup of tea, to gathering with your favourite people and watching the sunset from the deck of our boat, Lake Barrine has something for everyone.</p>
-      <p className="text-sm sm:text-md md:text-md lg:text-lg xl:text-xl text-custom-green 2xl:text-3xl">Then underneath that please change the 5km wording to:</p>
-      <p className="text-sm sm:text-md md:text-md lg:text-lg xl:text-xl text-custom-green 2xl:text-3xl">Explore the Rainforest with our 5km walking track through beautiful century old National
-Park.</p>
+      <h1 className="text-custom-green font-rufina font-bold py-6 xl:text-5xl 2xl:text-5xl sm:text-xl md:text-3xl lg:text-3xl xs:text-3xl text-center">Where moments become <span class="text-yellow-500">unforgettable memories</span></h1>
+      <p className="py-4 text-sm sm:text-md md:text-md lg:text-lg xl:text-2xl 2xl:text-2xl text-custom-green 2xl:text-3xl text-center">From cosy veranda chats over a hot cup of tea, to gathering with your favourite people and watching the sunset from the deck of our boat, Lake Barrine has something for everyone.</p>
+      
 
-      <div className="py-4">
+      <div className="py-4 flex justify-center">
 
     <Link to='/functions'>
     <button className="mb-10 md:mb-0 w-48 h-16 bg-[#3C5220] text-white rounded-lg text-lg cursor-pointer" onclick="window.location.href='URL_AQUI'">

1746d10802eb44c8d8e027029219b396b8f9af56
gpedrosad <gpedrosa@uade.edu.ar>
2024-07-05 19:05:19 -0300

    imagenes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path'; // Asegúrate de importar 'path' para manejar rutas
 
 export default defineConfig({
   base: '/lake_/', // Ajusta esto al nombre de tu repositorio
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: [
+        // Define aquí los recursos que deben tratarse como externos
+        '/public/images/down6.jpg',
+        '/public/images/down7.jpg'
+      ]
+    },
+    // Ajusta la dirección del directorio de activos si es necesario
+    assetsDir: path.resolve(__dirname, 'public'),
+  }
+});

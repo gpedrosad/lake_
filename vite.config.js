cdc7a4fd1df0a94d69e66e4c539a4187f3d3efb6
gpedrosad <gpedrosa@uade.edu.ar>
2024-07-05 19:46:19 -0300

    le saque el /

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/lake_/', // Esto ajusta el base path para recursos, adecuado para subdirectorios en producción
+  base: '/', // Esto ajusta el base path para recursos, adecuado para subdirectorios en producción
   plugins: [react()],
   build: {
     rollupOptions: {

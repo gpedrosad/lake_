24ae29486668a1dd19205772f4fc214a71923e56
gpedrosad <gpedrosa@uade.edu.ar>
2024-07-10 19:23:51 -0300

    cambio de mapa

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -5,7 +5,7 @@ const GoogleMap = () => {
     <div className="w-full md:w-3/4 lg:w-full mx-auto my-8 shadow-lg overflow-hidden rounded-lg">
       <iframe
         title="Google Map"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3809.879562980558!2d145.6114306!3d-17.2730607!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x69789ae6358d0a55%3A0xba771582e313e8e5!2sGillies%20Range%20Rd%2C%20Yungaburra%20QLD%204884%2C%20Australia!5e0!3m2!1ses-419!2sar!4v1720297992268!5m2!1ses-419!2sar"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3810.437344300034!2d145.6364761759596!3d-17.24606376037498!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x69789a244f490045%3A0x41798f7aa8c49235!2sLake%20Barrine%20Teahouse!5e0!3m2!1ses-419!2sar!4v1720650136383!5m2!1ses-419!2sar"
         width="100%"
         height="450"
         style={{ border: 0 }}

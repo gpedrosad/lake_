0c211190fbc30466c1357fdeb443d82dd16300c3
gpedrosad <gpedrosa@uade.edu.ar>
2024-07-23 00:11:43 -0300

    cambios contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -62,15 +62,57 @@ function Contact() {
                     <p>Our friendly team is here to assist with reservations, event inquiries, and more.</p>
                     <p>Reach out and let us help make your experience unforgettable.</p>
                 </div>
-
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                     <div className="bg-white shadow-md rounded-lg p-6">
                         <form className="space-y-4" onSubmit={handleSubmit}>
-                            {/* Form fields with handlers */}
+                            <div>
+                                <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
+                                <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} className="mt-1 block w-full rounded-md border border-custom-green px-4 h-12 focus:border-custom-green focus:ring focus:ring-custom-green focus:ring-opacity-50" placeholder="Your Name" />
+                            </div>
+                            <div className="flex flex-col md:flex-row gap-4">
+                                <div className="md:w-1/2">
+                                    <label htmlFor="email" className="block text-sm font-medium text-gray-700">E-mail</label>
+                                    <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} className="mt-1 block w-full rounded-md border border-custom-green px-4 h-12 focus:border-custom-green focus:ring focus:ring-custom-green focus:ring-opacity-50" placeholder="Your Email" />
+                                </div>
+                                <div className="md:w-1/2">
+                                    <label htmlFor="phone" className="block text-sm font-medium text-gray-700">Phone</label>
+                                    <input type="tel" id="phone" name="phone" value={formData.phone} onChange={handleChange} className="mt-1 block w-full rounded-md border border-custom-green px-4 h-12 focus:border-custom-green focus:ring focus:ring-custom-green focus:ring-opacity-50" placeholder="Your Phone Number" />
+                                </div>
+                            </div>
+                            <div>
+                                <label htmlFor="message" className="block text-sm font-medium text-gray-700">Message</label>
+                                <textarea id="message" name="message" value={formData.message} onChange={handleChange} rows="4" className="mt-1 block w-full rounded-md border border-custom-green px-4 focus:border-custom-green focus:ring focus:ring-custom-green focus:ring-opacity-50" placeholder="Your Message"></textarea>
+                            </div>
+                            <div className="text-center">
+                                <button type="submit" className="mt-2 px-4 py-2 bg-custom-green text-white rounded-md hover:bg-green-700 focus:outline-none focus:border-green-700 focus:ring focus:ring-green-500 focus:ring-opacity-50">
+                                    Submit
+                                </button>
+                            </div>
                         </form>
                     </div>
                     <div className="bg-white shadow-md rounded-lg p-6">
-                        {/* Contact information and social links */}
+                        {/* Social Links and other contact info */}
+                        <div className="grid grid-cols-8 grid-rows-5 items-center">
+                            <img src={Phone} alt="Phone icon" className="w-7 h-7 cursor-pointer col-start-1" />
+                            <p className="whitespace-nowrap col-start-2 col-span-7">(07) 4095 3847</p>
+                            <img src={Email} alt="Email icon" className="w-6 h-6 cursor-pointer col-start-1 row-start-3" />
+                            <p className="whitespace-nowrap col-start-2 col-span-7 row-start-3">info@lakebarrine.com.au</p>
+                            <a href="https://www.facebook.com/LakeBarrine" target="_blank" rel="noopener noreferrer" className="col-start-1 row-start-4">
+                                <FacebookIcon className="text-custom-green text-[40px] md:text-[30px]" />
+                                
+                            </a>
+                            <a href="https://www.instagram.com/lakebarrine" target="_blank" rel="noopener noreferrer" className="col-start-1 row-start-5">
+                                <InstagramIcon className="text-custom-green text-[40px] md:text-[30px]" />
+                            </a>
+                            <a href="https://maps.app.goo.gl/36dBFdTyxmWJEo3o7" target="_blank" rel="noopener noreferrer" className="col-start-1 row-start-2">                                
+                                <img src={map} alt="map icon" className="w-6 h-6 cursor-pointer col-start-1 row-start-5" />
+                            </a>
+                            <p className="whitespace-nowrap col-start-2 col-span-7 row-start-2">27 Lake Barrine Road, Yunguburra, QLD 4884</p>
+
+
+                            <p className="col-start-2 col-span-7 row-start-5">instagram.com/lakebarrine</p>
+                            <p className="col-start-2 col-span-7 row-start-4">facebook.com/lakebarrine</p>
+                        </div>
                     </div>
                 </div>
                 <div className="md:col-span-2 bg-white shadow-md rounded-lg p-6 mt-4">
